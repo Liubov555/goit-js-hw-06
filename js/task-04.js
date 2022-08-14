74eb9a71041e4773e829fc42582cea177e49e2c6
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-08-14 17:53:42 +0300

    Частково зроблено завдання 4

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+
+const counterValue = 0;
+
+
+const valueEl = document.querySelector('#value');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+
+incrementBtn.addEventListener('click', onIncrementValue);
+decrementBtn.addEventListener('click', onDecrementValue);
+
+function onIncrementValue(event) {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+}
+
+function onDecrementValue(event) {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
