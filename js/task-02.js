e919b89e63c46efc9878f3dbaa5746dfd3d91d19
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-08-14 10:17:07 +0300

    Виконане завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients')
+
+const items = ingredients.map(ingredient => {
+  const ingredientItem = document.createElement("li");
+  ingredientItem.textContent = `${ingredient}`;
+  ingredientItem.classList.add("item");
+  return ingredientItem;
+}
+);
+
+listEl.append(...items);
