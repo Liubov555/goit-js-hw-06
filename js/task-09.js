ba37899ef4b43fdc31f394f85e70e2afef176717
Liubov555 <101938652+Liubov555@users.noreply.github.com>
2022-08-20 11:28:01 +0300

    Виконане завдання 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+
+const bodyEl = document.querySelector('body');
+const btnEl = document.querySelector('.change-color')
+const colorTextEl = document.querySelector('.color')
+
+btnEl.addEventListener("click", onChangeColor);
+
+function onChangeColor(event) {
+  bodyEl.style.backgroundColor = `${getRandomHexColor()}`;
+  colorTextEl.textContent = `${getRandomHexColor()}`
+}
+
